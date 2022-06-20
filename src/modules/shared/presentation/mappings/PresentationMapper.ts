84b8ffeb6ff9cdaa4dc785b792e6a949d35fd547
anton-patrushev <anton11patrushev@gmail.com>
2022-06-20 22:04:55 +0300

    fix presentation mapper injection

--- src/modules/shared/presentation/mappings/PresentationMapper.ts
+++ src/modules/shared/presentation/mappings/PresentationMapper.ts
@@ -1,7 +1,9 @@
 import { IMapper } from 'modules/shared/common/mappings/IMapper';
 import { Mapper } from '@wufe/mapper';
 import { eventDomainToEventPresentation } from 'modules/events/presentation/mappings/maps/eventDomainToEventPresentation';
+import { injectable } from 'inversify';
 
+@injectable()
 export class PresentationMapper implements IMapper {
   public mapper: Mapper = new Mapper().withConfiguration((conf) =>
     conf.shouldIgnoreSourcePropertiesIfNotInDestination(true),
