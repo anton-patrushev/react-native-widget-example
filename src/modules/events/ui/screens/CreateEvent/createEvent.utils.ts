1d97e3aeb2df9957038c957e6c15b7750e4a17bd
anton-patrushev <anton11patrushev@gmail.com>
2022-06-29 01:38:16 +0300

    add createEvent.utils

--- src/modules/events/ui/screens/CreateEvent/createEvent.utils.ts
+++ src/modules/events/ui/screens/CreateEvent/createEvent.utils.ts
@@ -1,5 +1,9 @@
 import { Alert } from 'react-native';
 import format from 'date-fns/format';
+import addHours from 'date-fns/addHours';
+import subtractHours from 'date-fns/subHours';
+import isAfter from 'date-fns/isAfter';
+
 import { CreateEventDictionary } from 'modules/events/ui/screens/CreateEvent/createEvent.dictionary';
 
 export function formatEventTime(time: Date): string {
@@ -8,4 +12,15 @@ export function formatEventTime(time: Date): string {
 
 export function showErrorAlert(message: string) {
   Alert.alert(CreateEventDictionary.ERROR_ALERT_TITLE, message);
+}
+
+export function addOneHour(date: Date): Date {
+  return addHours(date, 1);
+}
+
+export function isBeforeForMoreThanOneHour(
+  date: Date,
+  compareDate: Date,
+): boolean {
+  return !isAfter(date, subtractHours(compareDate, 1));
 }
