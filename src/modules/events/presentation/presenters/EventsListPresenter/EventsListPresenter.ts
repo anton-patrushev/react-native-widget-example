44706966e39cd04be41b7f525df6f7c679ca592a
anton-patrushev <anton11patrushev@gmail.com>
2023-06-24 16:17:55 +0200

    feat: rewrite Presentation Layer mapping

--- src/modules/events/presentation/presenters/EventsListPresenter/EventsListPresenter.ts
+++ src/modules/events/presentation/presenters/EventsListPresenter/EventsListPresenter.ts
@@ -1,21 +1,15 @@
+import { map, Observable } from 'rxjs';
+
 import { IEventsListPresenter } from 'modules/events/presentation/presenters/EventsListPresenter/IEventsListPresenter';
 import { useInjection } from 'modules/shared/ioc/context/useInjection';
 import { IObserveAllEventsUseCase } from 'modules/events/domain/useCases/ObserveAllEventsUseCase/IObserveAllEventsUseCase';
 import { EventsModuleSymbols } from 'modules/events/ioc/symbols';
-import { map, Observable } from 'rxjs';
 import { Event } from 'modules/events/presentation/models/Event';
-import { Event as EventDomain } from 'modules/events/domain/models/Event';
-import { IMapper } from 'modules/shared/common/mappings/IMapper';
-import { SharedModuleSymbols } from 'modules/shared/ioc/symbols';
-import { PresentationMappingSignatures } from 'modules/events/presentation/mappings/signatures';
+import { EventPresentationMapper } from 'modules/events/presentation/mappers/EventPresentationMapper';
 
 export function useEventsListPresenter(): IEventsListPresenter {
   const observeAllEventsUseCase = useInjection<IObserveAllEventsUseCase>(
     EventsModuleSymbols.OBSERVE_ALL_EVENTS_USE_CASE,
-  );
-
-  const presentationMapper = useInjection<IMapper>(
-    SharedModuleSymbols.PRESENTATION_MAPPER,
   );
 
   const observeEvents = (): Observable<Array<Event>> => {
@@ -23,10 +17,7 @@ export function useEventsListPresenter(): IEventsListPresenter {
       .execute()
       .pipe(
         map((events) =>
-          presentationMapper.mapper.map<EventDomain[], Event[]>(
-            PresentationMappingSignatures.EVENT_DOMAIN_TO_EVENT_PRESENTATION,
-            events,
-          ),
+          events.map(EventPresentationMapper.mapFromDomainToPresentation),
         ),
       );
   };
--- src/modules/shared/ioc/index.ts
+++ src/modules/shared/ioc/index.ts
@@ -1,13 +1,7 @@
 import { DIModule } from 'modules/shared/ioc/types/modules';
-import { IMapper } from 'modules/shared/common/mappings/IMapper';
-import { PresentationMapper } from 'modules/shared/presentation/mappings/PresentationMapper';
-import { SharedModuleSymbols } from 'modules/shared/ioc/symbols';
 
 export const SharedModule: DIModule = {
-  init: (container): void => {
-    container
-      .bind<IMapper>(SharedModuleSymbols.PRESENTATION_MAPPER)
-      .to(PresentationMapper)
-      .inSingletonScope();
+  init: (_container): void => {
+    // TODO: init shared deps into DI container
   },
 };
